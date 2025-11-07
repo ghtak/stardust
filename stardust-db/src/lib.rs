064158107c21ea0650d6daec71536d48552e7604
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2025-11-07 20:41:21 +0900

    feat: add db with

--- stardust-db/src/lib.rs
+++ stardust-db/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod db;
 pub mod error;
 pub mod handle;
+pub mod with;
 
 // pub trait Context<'c>: sqlx::Executor<'c, Database = DefaultDriver> {}
 // impl<'c, T> Context<'c> for T where T: sqlx::Executor<'c, Database = DefaultDriver> {}
