da98654c0acc94766905c1b580e76c73ca8a5a1e
ghtak <gwonhyeong.tak@gmail.com>
2025-12-02 00:06:36 +0900

    refactor: remove unused Container struct and related code

--- appsample/src/container.rs
+++ appsample/src/container.rs
@@ -1,30 +1,3 @@
-// use std::sync::Arc;
-
-// pub struct Container<Database, UserContainer, OAuth2ServerContainer> {
-//     pub config: stardust_common::config::Config,
-//     pub database: Database,
-//     pub user_container: Arc<UserContainer>,
-//     pub oauth2_server_container: Arc<OAuth2ServerContainer>,
-// }
-
-// impl<Database, UserContainer, OAuth2ServerContainer>
-//     Container<Database, UserContainer, OAuth2ServerContainer>
-// {
-//     pub fn new(
-//         config: stardust_common::config::Config,
-//         database: Database,
-//         user_container: Arc<UserContainer>,
-//         oauth2_server_container: Arc<OAuth2ServerContainer>,
-//     ) -> Self {
-//         Self {
-//             config,
-//             database,
-//             user_container,
-//             oauth2_server_container,
-//         }
-//     }
-// }
-
 use crate::app::*;
 use std::sync::Arc;
 
